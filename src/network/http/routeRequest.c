67830925088d5d6aaee7fb2c4357d04c2a428bcd
grininmonkey <grininmonkey@sc.rr.com>
2025-04-15 11:16:53 -0400

    updates on 2025-04-15 11:16:53

--- src/network/http/routeRequest.c
+++ src/network/http/routeRequest.c
@@ -3,7 +3,7 @@
 #include "routeRequest.h"
 #include "queueErrorResponse.h"
 //-----------------------------------------------------
-//#include "handlers/values.h"
+//#include "handlers/???.h"
 #include "handlers/handleStatus.h"  
 #include "handlers/handleFile.h"
 //-----------------------------------------------------
@@ -12,22 +12,19 @@
 
 int route_request(const char *url, struct MHD_Connection *connection) {
 
-    // TODO: Add Handlers
+    // TODO: Add api(json) handlers
     if (strcmp(url, "/api/status") == 0)
         return handle_status(connection);
 
+    //TODO: determine content types, add generic handle_file call based on url var etc...
     if (strcmp(url, "/") == 0 || strcmp(url, "/index.html") == 0)
         return handle_File(connection,"/srv/rpiNode/html/index.html","text/html");
 
-    //TODO: determine content types, add generic handle_file call based on url var
 
-    //char filename[256];
-    //snprintf(filename, sizeof(filename), "%s", "");
 
-    // fallback -- unreached
+    // fallback for now -- unreached once generic file handler is implemented
     char error_msg[256];
     snprintf(error_msg, sizeof(error_msg), "%s&nbsp;:&nbsp;Not Found", url);
-
     return queue_error_response(connection, error_msg, MHD_HTTP_NOT_FOUND);
 
 }
