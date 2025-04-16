61229921009f2fd85fa67306bca5a0fa1e03fb57
grininmonkey <grininmonkey@sc.rr.com>
2025-04-16 15:33:53 -0400

    updates on 2025-04-16 15:33:53

--- src/sensors/MPU6050/readMPU6050.c
+++ src/sensors/MPU6050/readMPU6050.c
@@ -28,32 +28,37 @@ int read_word_2c(char high, char low) {
 }
 
 void* read_mpu6050(void* arg) {
-
+    //------------------------------------------------------------------------
+    // Top Vars
+    //------------------------------------------------------------------------
     int config_sleep_ms = 200; // default
     int file, MPU6050, MPU6050scanMilliseconds;
-    const char *bus = "/dev/i2c-1";
-
     pid_t t_pid = syscall(SYS_gettid);
-
+    const char *bus = "/dev/i2c-1";
+    //------------------------------------------------------------------------
+    // Get/Set settings
+    //------------------------------------------------------------------------
     pthread_mutex_lock(&rpiNode.lock);
-    // Get/Set settings 
-    if (namevalue_find_as_integer(rpiNode.internal_config, "MPU6050", &MPU6050) == 0)
-        MPU6050 = 0;
-    if (namevalue_find_as_integer(rpiNode.internal_config, "MPU6050scanMilliseconds", &MPU6050scanMilliseconds) == 0)
-        MPU6050scanMilliseconds = 200;
-    // Exit if not flagged
-    if (MPU6050 != 1) {
-        pthread_mutex_unlock(&rpiNode.lock);
-        return NULL;
-    }
-    // set sleep milliseconds
-    if (MPU6050scanMilliseconds > 20)
-        config_sleep_ms = MPU6050scanMilliseconds;
+        if (namevalue_find_as_integer(rpiNode.internal_config, "MPU6050", &MPU6050) == 0)
+            MPU6050 = 0;
+        if (namevalue_find_as_integer(rpiNode.internal_config, "MPU6050scanMilliseconds", &MPU6050scanMilliseconds) == 0)
+            MPU6050scanMilliseconds = 200;
+        // Exit if not flagged
+        if (MPU6050 != 1) {
+            pthread_mutex_unlock(&rpiNode.lock);
+            return NULL;
+        }
+        // set sleep milliseconds
+        if (MPU6050scanMilliseconds > 20)
+            config_sleep_ms = MPU6050scanMilliseconds;
     pthread_mutex_unlock(&rpiNode.lock);
-    
+    //------------------------------------------------------------------------
+    // Start / (notification)
+    //------------------------------------------------------------------------
     printf("[MPU6050][%d]: Started -> reading every (%i)ms\n", t_pid, config_sleep_ms);
-    
-    // open i2c
+    //------------------------------------------------------------------------
+    // Open i2c and connect to sensor
+    //------------------------------------------------------------------------
     if ((file = open(bus, O_RDWR)) < 0) {
         fprintf(stderr, "[MPU6050][%d]: Failed to open the i2c bus: %s\n", t_pid, strerror(errno));
         exit(1);
@@ -63,14 +68,15 @@ void* read_mpu6050(void* arg) {
         fprintf(stderr, "[MPU6050][%d]: Failed to connect to the sensor: %s\n", t_pid, strerror(errno));
         exit(1);
     }
-    
-    //Set MPU6050 values_count
+    //------------------------------------------------------------------------
+    // Set MPU6050 values_count (8) 
+    //------------------------------------------------------------------------
     pthread_mutex_lock(&rpiNode.lock);
-    rpiNode.internal.MPU6050.values_count = 8;
+        rpiNode.internal.MPU6050.values_count = 8;
     pthread_mutex_unlock(&rpiNode.lock);
-
-    char timestamp[RPI_MAX_META_TIMESTAMP];
-
+    //------------------------------------------------------------------------
+    // Vars needed
+    //------------------------------------------------------------------------
     float accel_x_buffer[WINDOW_SIZE] = {0};
     float accel_y_buffer[WINDOW_SIZE] = {0};
     float accel_z_buffer[WINDOW_SIZE] = {0};
@@ -78,21 +84,24 @@ void* read_mpu6050(void* arg) {
     int accel_x_index = 0;
     int accel_y_index = 0;
     int accel_z_index = 0;
-
+    //------------------------------------------------------------------------
     // Wake up MPU6050
+    //------------------------------------------------------------------------
     char config[2] = {0x6B, 0};
     write(file, config, 2);
-
+    //------------------------------------------------------------------------
     // Loop forever
+    //------------------------------------------------------------------------
     while (1) {
         pthread_mutex_lock(&lock);
-        if (!running) {
-            pthread_mutex_unlock(&lock);
-            break;
-        }
+            if (!running) {
+                pthread_mutex_unlock(&lock);
+                break;
+            }
         pthread_mutex_unlock(&lock);
-
+        //--------------------------------------------------------------------
         // Read 14 bytes from register 0x3B
+        //--------------------------------------------------------------------
         char reg = 0x3B;
         char data[14];
         write(file, &reg, 1);
@@ -100,30 +109,33 @@ void* read_mpu6050(void* arg) {
             printf("[MPU6050][%d]: Error reading sensor data\n", t_pid);
             continue;
         }
-
-        //Accelerometer raw
+        //--------------------------------------------------------------------
+        // Accelerometer raw
+        //--------------------------------------------------------------------
         int acc_x = read_word_2c(data[0], data[1]);
         int acc_y = read_word_2c(data[2], data[3]);
         int acc_z = read_word_2c(data[4], data[5]);
-
-        //Gyroscope (°/s)
+        //--------------------------------------------------------------------
+        // Gyroscope (°/s)
+        //--------------------------------------------------------------------
         float gyro_x = read_word_2c(data[8], data[9]) / 131.0;
         float gyro_y = read_word_2c(data[10], data[11]) / 131.0;
         float gyro_z = read_word_2c(data[12], data[13]) / 131.0;
 
-        //Accelerometer (g) smoothed 
+        //--------------------------------------------------------------------
+        // Accelerometer (g) smoothed 
+        //--------------------------------------------------------------------
         float pitch, roll;
         float smoothed_x = smoothing_moving_average(acc_x, accel_x_buffer, &accel_x_index) / 16384.0;
         float smoothed_y = smoothing_moving_average(acc_y, accel_y_buffer, &accel_y_index) / 16384.0;
         float smoothed_z = smoothing_moving_average(acc_z, accel_z_buffer, &accel_z_index) / 16384.0;
-
-        //Accelerometer (radians)
+        //--------------------------------------------------------------------
+        // Accelerometer (radians)
+        //--------------------------------------------------------------------
         calculate_angles(smoothed_x, smoothed_y, smoothed_z, &pitch, &roll);
-
-        //Set TimeStamp
-        SAFE_STRCPY(timestamp, get_timestamp());
-
+        //--------------------------------------------------------------------
         //Write to values structure
+        //--------------------------------------------------------------------
         mpu6050_assign_values(0, "pitch", pitch);
         mpu6050_assign_values(1, "roll", roll);
         mpu6050_assign_values(2, "accel-x", smoothed_x);
@@ -136,7 +148,9 @@ void* read_mpu6050(void* arg) {
         sleep_ms(config_sleep_ms); 
 
     }
-
+    //------------------------------------------------------------------------
+    // Finished
+    //------------------------------------------------------------------------
     close(file);
     printf("[MPU6050][%d]: Stopped\n", t_pid);
 
